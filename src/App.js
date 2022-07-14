b6c1e979a3a369ee43860d9000254d6714bc827b
AK Nahin <nahin@aknahin.com>
2022-07-14 14:30:02 +0600

    change

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import './App.css';
+import { useContext } from 'react';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Projects from './components/Projects/Projects';
+import ScrollToTop from './components/ScrollToTop/ScrollToTop';
+import Skills from './components/Skills/Skills';
+import { ThemeContext } from './contexts/theme';
 
 function App() {
+  const [{ themeName }] = useContext(ThemeContext)
+
   return (
-    <div className="App">
-      <h1>AK Nahin.com</h1>
+    <div id='top' className={`${themeName} app`}>
+      <Header />
+
+      <main>
+        <About />
+        <Projects />
+        <br />
+        <p><center>To see all projects visit behance profile. <a href='https://behance.com/aknahin'>aknahin</a></center> </p>
+        <Skills />
+        <Contact />
+      </main>
+
+      <ScrollToTop />
+      <Footer />
     </div>
   );
 }
